7658d8ecf783c0cba9173e431caec0b94c2314ff
Ray Donnelly <mingw.android@gmail.com>
2017-08-19 16:43:32 +0100

    Fix -std=c++17 to -std=c++14

--- clang/build.sh
+++ clang/build.sh
@@ -4,7 +4,7 @@ CHOST=${macos_machine}
 
 FINAL_CPPFLAGS="-D_FORTIFY_SOURCE=2 -mmacosx-version-min=${macos_min_version}"
 FINAL_CFLAGS="-march=core2 -mtune=haswell -mssse3 -ftree-vectorize -fPIC -fPIE -fstack-protector-strong -O2 -pipe"
-FINAL_CXXFLAGS="-march=core2 -mtune=haswell -mssse3 -ftree-vectorize -fPIC -fPIE -fstack-protector-strong -O2 -pipe -stdlib=libc++ -fvisibility-inlines-hidden -std=c++17 -fmessage-length=0"
+FINAL_CXXFLAGS="-march=core2 -mtune=haswell -mssse3 -ftree-vectorize -fPIC -fPIE -fstack-protector-strong -O2 -pipe -stdlib=libc++ -fvisibility-inlines-hidden -std=c++14 -fmessage-length=0"
 # These are the LDFLAGS for when the linker is being called directly, i.e. without -Wl,
 FINAL_LDFLAGS="-pie -headerpad_max_install_names"
 # These are the LDFLAGS for when the linker is being driven by a compiler, i.e. with -Wl,
