1c4a21439aab88b68c863bcb0cfddcfee772286b
Ray Donnelly <mingw.android@gmail.com>
2017-08-22 14:39:47 +0100

    libedit-feedstock: Build fixes

--- libedit-feedstock/recipe/build.sh
+++ libedit-feedstock/recipe/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-./configure --prefix=${PREFIX} --host=${HOST}
+./configure --prefix=${PREFIX}  \
+            --host=${HOST}      \
+            CFLAGS="${CFLAGS} -I${PREFIX}/include" \
+            LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 make -j ${CPU_COUNT}
 make install
