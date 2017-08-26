3287219a5b2d5fef67db95822fd169c34d34bb45
Michael Sarahan <msarahan@gmail.com>
2017-08-26 18:55:31 -0500

    fix mkl repack on windows with extra folder nesting

--- intel_repack/repack.sh
+++ intel_repack/repack.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+set -ex
 
 cp $SRC_DIR/$PKG_NAME/info/LICENSE.txt $SRC_DIR
 # for subpackages, we have named our extracted locations according to the subpackage name
 #    That's what this $PKG_NAME is doing - picking the right subfolder to rsync
-cp -rv "$SRC_DIR/$PKG_NAME/" "$PREFIX/"
+
+if [[ `uname` == "MINGW64_NT-10.0" ]]; then
+    src=$(cygpath -u "$SRC_DIR/$PKG_NAME/")
+    cp -rv $src/* "$PREFIX/"
+else
+    cp -rv "$SRC_DIR/$PKG_NAME/" "$PREFIX/"
+fi
+
 rm -rf $PREFIX/info
 
 if [[ `uname` == Darwin && -d $SRC_DIR/$PKG_NAME/lib ]]; then
