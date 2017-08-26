9fda0805f4f43f9ee39cdf710d6159701ffd9e9c
Ray Donnelly <mingw.android@gmail.com>
2017-08-26 13:24:01 +0100

    Pinning changes

--- crosstool-ng-feedstock/recipe/build.sh
+++ crosstool-ng-feedstock/recipe/build.sh
@@ -2,7 +2,10 @@
 
 export EXTRA_CFLAGS="-I${PREFIX}/include -I${PREFIX}/include/ncurses"
 export EXTRA_LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib -lncursesw"
-export CPPFLAGS="-I${PREFIX}/include -L${PREFIX}/lib"
+# -rpath-link is needed because libncursesw.so dependes on libtinfo.so and
+# configure will fail to find ncurses without it (if using the conda ncurses
+# package).
+export CPPFLAGS="-I${PREFIX}/include -L${PREFIX}/lib -Wl,-rpath-link,${PREFIX}/lib"
 if [[ $(uname) == Darwin ]]; then
   export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
 fi
