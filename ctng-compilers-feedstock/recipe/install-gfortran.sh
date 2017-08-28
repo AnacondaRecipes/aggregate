bf6c6c74285bad75f0366bca076e87135f6e4662
Ray Donnelly <mingw.android@gmail.com>
2017-08-28 07:49:55 +0100

    Fix bad merge (or something), more generalized logging

--- ctng-compilers-feedstock/recipe/install-gfortran.sh
+++ ctng-compilers-feedstock/recipe/install-gfortran.sh
@@ -54,3 +54,20 @@ chmod +x $PREFIX/bin/${CHOST}-gfortran
 mkdir -p ${PREFIX}/etc/conda/{de,}activate.d
 cp "${SRC_DIR}"/activate-gfortran.sh ${PREFIX}/etc/conda/activate.d/activate-${PKG_NAME}.sh
 cp "${SRC_DIR}"/deactivate-gfortran.sh ${PREFIX}/etc/conda/deactivate.d/deactivate-${PKG_NAME}.sh
+
+# Strip executables, we may want to install to a different prefix
+# and strip in there so that we do not change files that are not
+# part of this package.
+pushd ${PREFIX}
+  _files=$(find . -type f)
+  for _file in ${_files}; do
+    _type="$( file "${_file}" | cut -d ' ' -f 2- )"
+    case "${_type}" in
+      *script*executable*)
+      ;;
+      *executable*)
+        ${SRC_DIR}/gcc_built/bin/${CHOST}-strip --strip-all -v "${_file}"
+      ;;
+    esac
+  done
+popd
