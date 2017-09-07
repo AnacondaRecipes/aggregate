34c8f27cac174a460d3af0a7dbf3dd57e3939e73
Ray Donnelly <mingw.android@gmail.com>
2017-09-07 20:23:32 +0000

    Repackaging fixes for cmake-binary

--- cmake-binary-feedstock/recipe/build.sh
+++ cmake-binary-feedstock/recipe/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-pushd cmake-bin/CMake.app/Contents
-  mkdir ${PREFIX}/cmake-bin
-  rm -rf bin/cmake-gui
-  cp -rf bin doc man share ${PREFIX}/cmake-bin
-popd
+if [[ $(uname) == Darwin ]]; then
+  pushd cmake-bin/CMake.app/Contents
+    mkdir ${PREFIX}/cmake-bin
+    rm -rf bin/cmake-gui
+    cp -rf bin doc man share ${PREFIX}/cmake-bin
+  popd
+else
+  cp -rf cmake-bin ${PREFIX}/
+fi
