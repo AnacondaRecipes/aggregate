e828d4182f1ecb69bd6419738c1634a92a9c537f
Ray Donnelly <mingw.android@gmail.com>
2017-09-08 12:16:59 +0100

    Add jupyterlab for Windows too

--- _nb_ext_conf/recipe/bld.bat
+++ _nb_ext_conf/recipe/bld.bat
@@ -24,5 +24,9 @@ if errorlevel 1 exit 1
 jupyter-nbextension enable widgetsnbextension --py --sys-prefix
 if errorlevel 1 exit 1
 
+:: jupyterlab
+jupyter-serverextension enable jupyterlab --py --sys-prefix
+if errorlevel 1 exit 1
+
 :: ensure we don't ship any pyc's
 rd /s /q %SP_DIR%
