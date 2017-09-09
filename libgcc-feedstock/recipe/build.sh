5799e3249e1a9179099f4d58e5335abe88698abe
Ray Donnelly <mingw.android@gmail.com>
2017-09-09 21:30:33 +0000

    Cannot symlink libgfortran.3 to .4 on Linux

--- libgcc-feedstock/recipe/build.sh
+++ libgcc-feedstock/recipe/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 if [[ $(uname) == Linux ]]; then
-  ln -s ${PREFIX}/lib/libgfortran.so.4.0.0 ${PREFIX}/lib/libgfortran.so.3
-  ln -s ${PREFIX}/lib/libgfortran.so.4.0.0 ${PREFIX}/lib/libgfortran.so.3.0
   ln -s ${PREFIX}/lib/libstdc++.so.6.0.24 ${PREFIX}/lib/libstdc++.so.6.0.21
 fi
