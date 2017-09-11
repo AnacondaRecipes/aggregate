455750156e159269b9f84bf4f5a9f782274cbcfe
Ray Donnelly <mingw.android@gmail.com>
2017-09-11 10:22:58 +0000

    dos2unix on cmake-binary-feedstock and a few updates

--- cmake-binary-feedstock/recipe/build.sh
+++ cmake-binary-feedstock/recipe/build.sh
@@ -8,4 +8,13 @@ if [[ $(uname) == Darwin ]]; then
   popd
 else
   cp -rf cmake-bin ${PREFIX}/
+  [[ -d curl-bin ]]    && cp -rf curl-bin/*    ${PREFIX}/cmake-bin/
+  [[ -d expat-bin ]]   && cp -rf expat-bin/*   ${PREFIX}/cmake-bin/
+  [[ -d krb5-bin ]]    && cp -rf krb5-bin/*    ${PREFIX}/cmake-bin/
+  [[ -d libssh2-bin ]] && cp -rf libssh2-bin/* ${PREFIX}/cmake-bin/
+  [[ -d ncurses-bin ]] && cp -rf ncurses-bin/* ${PREFIX}/cmake-bin/
+  [[ -d openssl-bin ]] && cp -rf openssl-bin/* ${PREFIX}/cmake-bin/
+  [[ -d xz-bin ]]      && cp -rf xz-bin/*      ${PREFIX}/cmake-bin/
+  [[ -d zlib-bin ]]    && cp -rf zlib-bin/*    ${PREFIX}/cmake-bin/
+  [[ -d bzip2-bin ]]   && cp -rf bzip2-bin/*   ${PREFIX}/cmake-bin/
 fi
