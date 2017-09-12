72b45a7b322177da349d020021a9dadc353c2a8c
Ray Donnelly <mingw.android@gmail.com>
2017-09-12 12:44:39 +0000

    Even better fix

--- cmake-binary-feedstock/recipe/build.sh
+++ cmake-binary-feedstock/recipe/build.sh
@@ -6,16 +6,17 @@ if [[ $(uname) == Darwin ]]; then
     rm -rf bin/cmake-gui
     cp -rf bin doc man share ${PREFIX}/cmake-bin
   popd
-elif [[ $(uname) == Linux ]]; then
-  cp -rf cmake-bin ${PREFIX}/
 else
-  [[ -d curl-bin ]]    && cp -rf curl-bin/*    ${PREFIX}/cmake-bin/
-  [[ -d expat-bin ]]   && cp -rf expat-bin/*   ${PREFIX}/cmake-bin/
-  [[ -d krb5-bin ]]    && cp -rf krb5-bin/*    ${PREFIX}/cmake-bin/
-  [[ -d libssh2-bin ]] && cp -rf libssh2-bin/* ${PREFIX}/cmake-bin/
-  [[ -d ncurses-bin ]] && cp -rf ncurses-bin/* ${PREFIX}/cmake-bin/
-  [[ -d openssl-bin ]] && cp -rf openssl-bin/* ${PREFIX}/cmake-bin/
-  [[ -d xz-bin ]]      && cp -rf xz-bin/*      ${PREFIX}/cmake-bin/
-  [[ -d zlib-bin ]]    && cp -rf zlib-bin/*    ${PREFIX}/cmake-bin/
-  [[ -d bzip2-bin ]]   && cp -rf bzip2-bin/*   ${PREFIX}/cmake-bin/
+  cp -rf cmake-bin ${PREFIX}/
+  if [[ $(uname) == Linux ]] && [[ $(uname -m) == i686 ]]; then
+    [[ -d curl-bin ]]    && cp -rf curl-bin/*    ${PREFIX}/cmake-bin/
+    [[ -d expat-bin ]]   && cp -rf expat-bin/*   ${PREFIX}/cmake-bin/
+    [[ -d krb5-bin ]]    && cp -rf krb5-bin/*    ${PREFIX}/cmake-bin/
+    [[ -d libssh2-bin ]] && cp -rf libssh2-bin/* ${PREFIX}/cmake-bin/
+    [[ -d ncurses-bin ]] && cp -rf ncurses-bin/* ${PREFIX}/cmake-bin/
+    [[ -d openssl-bin ]] && cp -rf openssl-bin/* ${PREFIX}/cmake-bin/
+    [[ -d xz-bin ]]      && cp -rf xz-bin/*      ${PREFIX}/cmake-bin/
+    [[ -d zlib-bin ]]    && cp -rf zlib-bin/*    ${PREFIX}/cmake-bin/
+    [[ -d bzip2-bin ]]   && cp -rf bzip2-bin/*   ${PREFIX}/cmake-bin/
+  fi
 fi
