21749924c02bc7c4ebd50293b7a4b6440897e07c
Ray Donnelly <mingw.android@gmail.com>
2017-09-15 08:29:29 -0700

    Fix portaudio bugs

--- portaudio-feedstock/recipe/build.sh
+++ portaudio-feedstock/recipe/build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-./configure --with-pic --prefix=${PREFIX}
+declare -a CONFIG_OPTS
+CONFIG_OPTS+=(--prefix=${PREFIX})
+CONFIG_OPTS+=(--with-pic)
+if [[ -n "${CONDA_BUILD_SYSROOT}" ]]; then
+  CONFIG_OPTS+=(--with-sysroot="${CONDA_BUILD_SYSROOT}")
+fi
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  # Overrides the sysroot with an old one.
+  CONFIG_OPTS+=(--disable-mac-universal)
+  export CFLAGS="${CFLAGS} -Wno-deprecated-declarations"
+fi
+
+./configure "${CONFIG_OPTS[@]}"
 make
 make install
 
