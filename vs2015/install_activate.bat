b6828429a9d8043a5971a52e1dc02eb66e19ab43
Ray Donnelly <mingw.android@gmail.com>
2017-09-18 15:27:44 +0100

    vs2015: Be explicit about Windows 10 SDK to use when calling vcvarsall.bat

--- vs2015/install_activate.bat
+++ vs2015/install_activate.bat
@@ -12,4 +12,4 @@ IF "%cross_compiler_target_platform%" == "win-64" (
     echo SET "CMAKE_GENERATOR=Visual Studio %VER% %YEAR%" >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
   )
 
-echo CALL "%%VSINSTALLDIR%%\..\..\VC\vcvarsall.bat" %target_platform% >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
+echo CALL "%%VSINSTALLDIR%%..\..\VC\vcvarsall.bat" %target_platform% 10.0.10586.0 >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
