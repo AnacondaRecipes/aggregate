12419a30b54ef011415a61a82239b3500ff36c86
Michael Sarahan <msarahan@gmail.com>
2017-10-25 15:00:53 -0500

    replace intel_repack with a submodule to facilitate more versions

--- intel_repack/install-devel.sh
+++
@@ -1,9 +0,0 @@
-if [[ `uname` == "MINGW64_NT-10.0" ]]; then
-    cp $RECIPE_DIR/site.cfg $PREFIX/site.cfg
-    echo library_dirs = $(cygpath -w $LIBRARY_LIB)  >> $PREFIX/site.cfg
-    echo include_dirs = $(cygpath -w $LIBRARY_INC) >> $PREFIX/site.cfg
-else
-    cp $RECIPE_DIR/site.cfg $PREFIX/site.cfg
-    echo library_dirs = $PREFIX/lib  >> $PREFIX/site.cfg
-    echo include_dirs = $PREFIX/include  >> $PREFIX/site.cfg
-fi
