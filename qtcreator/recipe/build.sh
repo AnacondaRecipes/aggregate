1ac165116c4bd01b1888b59d9f0712ecf136b5c8
Ray Donnelly <mingw.android@gmail.com>
2017-11-17 01:23:48 +0000

    Updates for QtCreator on Linux

--- qtcreator/recipe/build.sh
+++ qtcreator/recipe/build.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
 # Avoid Xcode
-mkdir xcode
-pushd xcode
-  cp "${RECIPE_DIR}"/xcrun .
-  cp "${RECIPE_DIR}"/xcodebuild .
-  PATH=${PWD}:${PATH}
-popd
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  mkdir xcode
+  pushd xcode
+    cp "${RECIPE_DIR}"/xcrun .
+    cp "${RECIPE_DIR}"/xcodebuild .
+    PATH=${PWD}:${PATH}
+  popd
+fi
 
 # This appears in the "About" dialog, but qmake is not good and I cannot
 # find any way to prevent it getting mangled (-DAnaconda -DBuild ...)
 echo DEFINES += IDE_VERSION_DESCRIPTION=\\\"Anaconda Build ${PKG_BUILDNUM}\\\" >> qtcreator.pro
-qmake -r qtcreator.pro QTC_PREFIX=/ QBS_INSTALL_PREFIX=/
-# Work-around a race condition on OS X? "make[2]: write error"
-make -j${CPU_COUNT} || make -j${CPU_COUNT} || make
+qmake -r qtcreator.pro                   \
+      QTC_PREFIX=/                       \
+      QBS_INSTALL_PREFIX=/               \
+      QMAKE_CC=${CC}                     \
+      QMAKE_CXX=${CXX}                   \
+      QMAKE_LINK=${CXX}                  \
+      QMAKE_LINK_C=${CC}                 \
+      QMAKE_CXXFLAGS="${CXXFLAGS}"       \
+      QMAKE_LFLAGS_RELEASE="${LDFLAGS}"
+make -j${CPU_COUNT}
 make install INSTALL_ROOT=${PREFIX}
