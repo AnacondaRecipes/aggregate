b78c421f59b3445a254801983d4b10ea007fd85b
Jonathan Helmus <jjhelmus@gmail.com>
2017-11-22 13:42:18 -0600

    add pytorch 0.2.0 recipe

---
+++ pytorch-feedstock/recipe/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export CFLAGS="${CFLAGS} -I${PREFIX}/include"
+export CMAKE_LIBRARY_PATH=$PREFIX/lib:$PREFIX/include:$CMAKE_LIBRARY_PATH
+export CMAKE_PREFIX_PATH=$PREFIX
+# compile for Kepler, Kepler+Tesla, Maxwell
+# 3.0, 3.5, 5.0, 5.2+PTX
+export TORCH_CUDA_ARCH_LIST="3.0;3.5;5.0;5.2+PTX"
+export TORCH_NVCC_FLAGS="-Xfatbin -compress-all"
+export PYTORCH_BINARY_BUILD=1
+export TH_BINARY_BUILD=1
+export PYTORCH_BUILD_VERSION=$PKG_VERSION
+export PYTORCH_BUILD_NUMBER=$PKG_BUILDNUM
+python setup.py install
