c3763ba64d86f8f11a74be20bb1d1ae8e8b2d172
Ray Donnelly <mingw.android@gmail.com>
2017-11-24 15:58:33 +0000

    qtcreator: .app bundle / navigator app stuff

--- qtcreator-feedstock/recipe/build.sh
+++ qtcreator-feedstock/recipe/build.sh
@@ -3,7 +3,7 @@
 # We will have cloned the qbs submodule. Remove it.
 rm -rf src/shared/qbs || true
 
-if [[ ! -f ${PREFIX}/bin/llvm-config ]]; then
+if [[ ! -f "${PREFIX}"/bin/llvm-config ]]; then
   echo "You need to add a host dep of llvmdev for the Clang Code Model"
   exit 1
 fi
@@ -11,7 +11,7 @@ fi
 # Avoid:
 # ${PREFIX}/include/sqlite3.h:654:8: error: redefinition of 'struct sqlite3_file'
 #  struct sqlite3_file
-rm -f ${PREFIX}/include/sqlite3.h
+rm -f "${PREFIX}"/include/sqlite3.h
 
 # Avoid Xcode
 if [[ ${HOST} =~ .*darwin.* ]]; then
@@ -27,9 +27,9 @@ fi
 # find any way to prevent it getting mangled (-DAnaconda -DBuild ...)
 echo DEFINES += IDE_VERSION_DESCRIPTION=\\\"Anaconda Build ${PKG_BUILDNUM}\\\" >> qtcreator.pro
 qmake -r qtcreator.pro                   \
-      QTC_PREFIX=${PREFIX}               \
-      QBS_INSTALL_PREFIX=${PREFIX}       \
-      LLVM_INSTALL_DIR=${PREFIX}         \
+      QTC_PREFIX="${PREFIX}"             \
+      QBS_INSTALL_PREFIX="${PREFIX}"     \
+      LLVM_INSTALL_DIR="${PREFIX}"       \
       QMAKE_CC=${CC}                     \
       QMAKE_CXX=${CXX}                   \
       QMAKE_LINK=${CXX}                  \
@@ -38,3 +38,22 @@ qmake -r qtcreator.pro                   \
       QMAKE_LFLAGS_RELEASE="${LDFLAGS}"
 make -j${CPU_COUNT}
 make install
+
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  mkdir -p "${PREFIX}"/qtcreatorapp/Contents/Resources/
+  cp -rf src/app/qtcreator.xcassets/qtcreator.appiconset src/app/qtcreator.xcassets/qtcreator.iconset
+  iconutil -c icns -o "${PREFIX}"/qtcreatorapp/Contents/Resources/qtcreator.icns src/app/qtcreator.xcassets/qtcreator.iconset
+  iconutil -c icns -o "${PREFIX}"/qtcreatorapp/Contents/Resources/profile.icns src/app/qtcreator.xcassets/profile.iconset
+  iconutil -c icns -o "${PREFIX}"/qtcreatorapp/Contents/Resources/prifile.icns src/app/qtcreator.xcassets/prifile.iconset
+  cat src/app/Info.plist | sed                                                   \
+    -e "s|<string>10.8</string>|<string>${MACOSX_DEPLOYMENT_TARGET}</string>|g"  \
+    -e "s|@FULL_VERSION@|${PKG_VERSION}|g"                                       \
+    -e "s|@SHORT_VERSION@|${PKG_VERSION}|g"                                      \
+    -e "s|<string>Qt Creator</string>|<string>qtcreator</string>|g"              \
+    > "${PREFIX}"/qtcreatorapp/Contents/Info.plist
+  cp "${RECIPE_DIR}"/osx-post.sh "${PREFIX}"/bin/.qtcreator-post-link.sh
+  cp "${RECIPE_DIR}"/osx-pre.sh "${PREFIX}"/bin/.qtcreator-pre-unlink.sh
+elif [[ ${HOST} =~ .*linux.* ]]; then
+  echo "It would be nice to add a .desktop file here, but it would"
+  echo "be even nicer if menuinst handled both that and App bundles."
+fi
