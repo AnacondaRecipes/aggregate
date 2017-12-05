09e28706fbb1d4535edb48b14661fa4bc1f94828
Kale Franz <kfranz@continuum.io>
2017-12-05 14:04:17 -0600

    ruamel-feedstock namespace package

---
+++ ruamel-feedstock/recipe/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="ruamel",
+    version="0.1.0",
+    author="Anaconda, Inc.",
+    license="MIT",
+    description="Namespace package for ruamel namespace.",
+    packages=("ruamel",),
+    zip_safe=False,
+)
