70a81fe78666f77519b04476e8d9f65c546a831e
Nehal J Wani <nehaljw.kkd1@gmail.com>
2018-01-29 11:38:41 -0500

    Add more CDT packages for ppc64le

---
+++ gettext-devel-cos7-ppc64le/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+RPM=$(find ${PWD}/binary -name "*.rpm")
+mkdir -p ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot
+pushd ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot > /dev/null 2>&1
+  "${RECIPE_DIR}"/rpm2cpio "${RPM}" | cpio -idmv
+popd > /dev/null 2>&1
