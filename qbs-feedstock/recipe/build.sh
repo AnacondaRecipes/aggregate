290fb09fc92ddb4d292c4fb27eae9ece9be8b307
Ray Donnelly <mingw.android@gmail.com>
2018-03-06 13:57:21 -0800

    qbs: Avoid xcode using qt xc scripts

--- qbs-feedstock/recipe/build.sh
+++ qbs-feedstock/recipe/build.sh
@@ -2,21 +2,16 @@
 
 # Avoid Xcode
 if [[ ${HOST} =~ .*darwin.* ]]; then
-  mkdir xcode
-  pushd xcode
-    cp "${RECIPE_DIR}"/xcrun .
-    cp "${RECIPE_DIR}"/xcodebuild .
-    PATH=${PWD}:${PATH}
-  popd
+  PATH=${PREFIX}/bin/xc-avoidance:${PATH}
 fi
 
-qmake -r qbs.pro                         \
-      QBS_INSTALL_PREFIX=${PREFIX}       \
-      QMAKE_CC=${CC}                     \
-      QMAKE_CXX=${CXX}                   \
-      QMAKE_LINK=${CXX}                  \
-      QMAKE_LINK_C=${CC}                 \
-      QMAKE_CXXFLAGS="${CXXFLAGS}"       \
+qmake -r qbs.pro                       \
+      QBS_INSTALL_PREFIX=${PREFIX}     \
+      QMAKE_CC=${CC}                   \
+      QMAKE_CXX=${CXX}                 \
+      QMAKE_LINK=${CXX}                \
+      QMAKE_LINK_C=${CC}               \
+      QMAKE_CXXFLAGS="${CXXFLAGS}"     \
       QMAKE_LFLAGS_RELEASE="${LDFLAGS}"
 make -j${CPU_COUNT}
 make install
