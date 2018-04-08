20449ee9c838c10f5fedac84620bf906746c390b
Ray Donnelly <mingw.android@gmail.com>
2018-04-09 00:30:22 +0100

    Some fixes for Unix and WIP Windows work

--- qt3dstudio-feedstock/recipe/bld.bat
+++ qt3dstudio-feedstock/recipe/bld.bat
@@ -1,6 +1,18 @@
-:: This appears in the "About" dialog, but qmake is not good and I cannot
-:: find any way to prevent it getting mangled (-DAnaconda -DBuild ...)
-:: echo DEFINES += IDE_VERSION_DESCRIPTION=\"Anaconda Build %PKG_BUILDNUM%\" >> qtcreator.pri
-qmake -r qtcreator.pro CONFIG+=release QTC_PREFIX=/Library QBS_INSTALL_PREFIX=/Library
+:: Apply submodule patch
+pushd src/Runtime/Qt3DSRuntime
+  git am %RECIPE_DIR%/0001-linux-Need-lrt-for-clock_gettime.patch
+popd
+
+if not exist build mkdir build
+pushd build
+
+if not exist Makefile qmake -r ..\qt3dstudio.pro
 jom
-jom install INSTALL_ROOT=%PREFIX%
+jom -f Makefile install
+jom -f Makefile sub-examples-install_subtargets
+if not exist %PREFIX%\share\qt3dstudio mkdir %PREFIX%\share\qt3dstudio
+rename %PREFIX%\Library\examples %PREFIX%\Library\share
+rename %PREFIX%\Library\share\studio3d %PREFIX%\Library\share\qt3dstudio
+xcopy /f /s /y /i public-demos\* %PREFIX%\Library\share\qt3dstudio
+
+exit 1
