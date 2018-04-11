b7579119655fd18143f419c2b943830e9ce4d86d
Michael Sarahan <msarahan@gmail.com>
2018-04-11 10:23:04 -0500

    update VS2015 and VS2017 to fix runtimes

--- vs2017/install_runtime.bat
+++ vs2017/install_runtime.bat
@@ -7,8 +7,11 @@ set MSC_VER=2017
 
 REM ========== REQUIRES Win 10 SDK be installed, or files otherwise copied to location below!
 robocopy "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\%VC_PATH%"  "%LIBRARY_BIN%" *.dll /E
+if %ERRORLEVEL% GEQ 8 exit 1
 robocopy "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\%VC_PATH%"  "%PREFIX%" *.dll /E
 if %ERRORLEVEL% GEQ 8 exit 1
+COPY "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\%VC_PATH%\ucrtbase.dll" "%LIBRARY_BIN%"
+COPY "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\%VC_PATH%\ucrtbase.dll" "%PREFIX%"
 
 REM ========== This one comes from visual studio 2017
 set "UPDATE_VER=14.12.25810"
@@ -24,7 +27,7 @@ if not exist "%BT_ROOT%" (
 set "BT_ROOT=C:\Program Files (x86)\Microsoft Visual Studio\%MSC_VER%\Professional"
 )
 
-set "REDIST_ROOT=%BT_ROOT%\VC\Redist\MSVC\%UPDATE_VER%\onecore\%VC_PATH%"
+set "REDIST_ROOT=%BT_ROOT%\VC\Redist\MSVC\%UPDATE_VER%\%VC_PATH%"
 robocopy "%REDIST_ROOT%\Microsoft.VC%VC_VER%.CRT" "%LIBRARY_BIN%" *.dll /E
 if %ERRORLEVEL% GTR 8 exit 1
 robocopy "%REDIST_ROOT%\Microsoft.VC%VC_VER%.CRT" "%PREFIX%" *.dll /E
@@ -33,3 +36,4 @@ robocopy "%REDIST_ROOT%\Microsoft.VC%VC_VER%.OpenMP" "%LIBRARY_BIN%" *.dll /E
 if %ERRORLEVEL% GTR 8 exit 1
 robocopy "%REDIST_ROOT%\Microsoft.VC%VC_VER%.OpenMP" "%PREFIX%" *.dll /E
 if %ERRORLEVEL% GTR 8 exit 1
+
