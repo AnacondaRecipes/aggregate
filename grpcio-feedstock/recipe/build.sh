3fbeb0fb3a0510adfc07f5a7b65790fd5301e71d
Jonathan Helmus <jjhelmus@gmail.com>
2018-05-02 06:08:45 -0500

    grpcio: add support for osx

--- grpcio-feedstock/recipe/build.sh
+++ grpcio-feedstock/recipe/build.sh
@@ -8,4 +8,18 @@ rm -rf ${PREFIX}/include/openssl*
 export GRPC_PYTHON_CFLAGS=""
 export GRPC_PYTHON_LDFLAGS=""
 
+if [[ ${HOST} =~ .*darwin.* ]]; then
+    # the makefile uses $AR for creating libraries, set it correctly here
+    export AR="$LIBTOOL -no_warning_for_no_symbols -o"
+
+    # we need a single set of openssl include files, the ones in PREFIX were
+    # removed above
+    export CFLAGS="${CFLAGS} -I./third_party/boringssl/include"
+    export CXXFLAGS="${CXXFLAGS} -I./third_party/boringssl/include"
+
+    # the Python extension has an unresolved _deflate symbol, link to libz to
+    # resolve
+    export LDFLAGS="${LDFLAGS} -lz"
+fi
+
 python setup.py install --single-version-externally-managed --record=record.txt
