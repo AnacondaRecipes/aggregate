5484ad9ee7a3375116e82d702bbf54f0af8a1be3
Nehal J Wani <nehaljw.kkd1@gmail.com>
2018-06-02 10:13:51 -0500

    Update svn to 1.10.0
    - Use git format patches
    - Add patch to fix pdb file locations
    - Add external dep libutf8proc
    - Make build/host split better
    - Drop likages check, rely on pyldd

--- svn-feedstock/recipe/build.sh
+++ svn-feedstock/recipe/build.sh
@@ -2,9 +2,13 @@
 
 ./configure \
     --prefix=$PREFIX \
+    --host=$HOST \
     --with-sqlite=$PREFIX \
     --with-zlib=$PREFIX \
-    --with-serf=$PREFIX
+    --with-serf=$PREFIX \
+    --with-lz4=$PREFIX \
+    --with-utf8proc=$PREFIX \
+    --with-expat=$PREFIX/include:$PREFIX/lib:expat
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
