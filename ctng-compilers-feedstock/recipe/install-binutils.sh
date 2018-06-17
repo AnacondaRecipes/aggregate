bfc340ec014f1d61f8aee54335e09689b1a110de
Nehal J Wani <nehaljw.kkd1@gmail.com>
2018-06-17 04:47:25 +0000

    binutils: use install-strip target instead of implementing it

--- ctng-compilers-feedstock/recipe/install-binutils.sh
+++ ctng-compilers-feedstock/recipe/install-binutils.sh
@@ -4,7 +4,7 @@ CHOST=$(${SRC_DIR}/.build/*-*-*-*/build/build-cc-gcc-final/gcc/xgcc -dumpmachine
 
 pushd ${SRC_DIR}/.build/${CHOST}/build/build-binutils-host-*
   PATH=${SRC_DIR}/.build/${CHOST}/buildtools/bin:$PATH \
-  make prefix=${PREFIX} install
+  make prefix=${PREFIX} install-strip
 popd
 
 # Copy the liblto_plugin.so from the build tree. This is something of a hack and, on OSes other
@@ -12,21 +12,4 @@ popd
 pushd ${SRC_DIR}/.build/*-*-*-*/build/build-cc-gcc-core-pass-2/gcc/
   mkdir -p ${PREFIX}/libexec/gcc/${CHOST}/${TOP_PKG_VERSION}/
   cp -a liblto* ${PREFIX}/libexec/gcc/${CHOST}/${TOP_PKG_VERSION}/
-popd
-
-# Strip executables, we may want to install to a different prefix
-# and strip in there so that we do not change files that are not
-# part of this package.
-pushd ${PREFIX}
-  _files=$(find . -type f)
-  for _file in ${_files}; do
-    _type="$( file "${_file}" | cut -d ' ' -f 2- )"
-    case "${_type}" in
-      *script*executable*)
-      ;;
-      *executable*)
-        ${SRC_DIR}/gcc_built/bin/${CHOST}-strip --strip-all -v "${_file}"
-      ;;
-    esac
-  done
 popd
