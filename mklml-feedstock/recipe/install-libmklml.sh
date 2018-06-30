7dc99fa71831edac5fbabe7637951c94d6e627b6
Nehal J Wani <nehaljw.kkd1@gmail.com>
2018-06-30 05:22:47 -0500

    mklml: copy mklml_gnu too (linux)

--- mklml-feedstock/recipe/install-libmklml.sh
+++ mklml-feedstock/recipe/install-libmklml.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-if [[ ${HOST} =~ .*darwin.* ]]; then
-  cp mklml/lib/libmklml${SHLIB_EXT} $PREFIX/lib
-else
-  cp mklml/lib/libmklml_intel${SHLIB_EXT} $PREFIX/lib
-fi
+cp mklml/lib/libmklml*${SHLIB_EXT} $PREFIX/lib
