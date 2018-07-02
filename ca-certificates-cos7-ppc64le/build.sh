8e819eec436d7c5516a3527a8c7fc6aa841b0f9b
Ray Donnelly <mingw.android@gmail.com>
2018-07-02 17:39:44 +0100

    PowerPC CDTs: Hack more symlink fixes that should maybe be generalized

--- ca-certificates-cos7-ppc64le/build.sh
+++ ca-certificates-cos7-ppc64le/build.sh
@@ -13,3 +13,26 @@ pushd ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot > /dev/null 2>&1
     cp -Rf "${SRC_DIR}"/binary/* .
   fi
 popd > /dev/null 2>&1
+
+pushd ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot/etc/pki/java
+  rm -f cacerts
+  mkdir -p ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot/etc/pki/ca-trust/extracted/java/cacerts
+  ln -s ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot/etc/pki/ca-trust/extracted/java/cacerts cacerts
+popd
+
+pushd ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot/etc/pki/tls
+  rm -f cert.pem
+  echo "PLACEHOLDER"> ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem
+  ln -s ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem cert.pem
+popd
+
+pushd ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot/etc/pki/tls/certs
+  rm -f ca-bundle.crt
+  ln -s ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem ca-bundle.crt
+popd
+
+pushd ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot/etc/pki/tls/certs
+  rm ca-bundle.trust.crt
+  echo "PLACEHOLDER"> ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot/etc/pki/ca-trust/extracted/openssl/ca-bundle.trust.crt
+  ln -s ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot/etc/pki/ca-trust/extracted/openssl/ca-bundle.trust.crt ca-bundle.trust.crt
+popd
