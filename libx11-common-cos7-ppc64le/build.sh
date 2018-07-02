a19b5e4afc434476579b816a222c768489c9ee9d
Ray Donnelly <mingw.android@gmail.com>
2018-07-02 12:14:17 +0100

    Regenerate ppc64le cos7 CDT packages

--- libx11-common-cos7-ppc64le/build.sh
+++ libx11-common-cos7-ppc64le/build.sh
@@ -3,13 +3,13 @@
 set -e
 
 mkdir -p ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot
-RPMS=$(find "${SRC_DIR}"/binary -name "*.rpm" -printf "." | wc -c)
-RPM=$(find "${SRC_DIR}"/binary -name "*.rpm")
-TOTAL=$(find "${SRC_DIR}"/binary -type f -printf "." | wc -c)
-  pushd ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot > /dev/null 2>&1
-if [[ ${RPMS} == 1 ]] && [[ ${TOTALS} == 1 ]] && [[ -f ${RPM} ]]; then
-  "${RECIPE_DIR}"/rpm2cpio "${RPM}" | cpio -idmv
-else
-  cp -Rf "${SRC_DIR}"/binary/* .
-fi
+RPMS=$(find "${SRC_DIR}"/binary -name "*.rpm" -exec printf "." \; | wc -c | tr -d " ")
+THE_RPM=$(find "${SRC_DIR}"/binary -name "*.rpm")
+TOTAL=$(find "${SRC_DIR}"/binary -type f -exec printf "." \; | wc -c | tr -d " ")
+pushd ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot > /dev/null 2>&1
+  if [[ ${RPMS} == 1 ]] && [[ ${TOTAL} == 1 ]] && [[ -f ${THE_RPM} ]]; then
+    "${RECIPE_DIR}"/rpm2cpio "${THE_RPM}" | cpio -idmv
+  else
+    cp -Rf "${SRC_DIR}"/binary/* .
+  fi
 popd > /dev/null 2>&1
