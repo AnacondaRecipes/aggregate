dda33f3fbb022b77949c354942b8f1284dd540d1
Ray Donnelly <mingw.android@gmail.com>
2018-07-02 06:13:28 -0400

    Update for libarchive

--- libx11-common-cos7-ppc64le/build.sh
+++ libx11-common-cos7-ppc64le/build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-RPM=$(find ${PWD}/binary -name "*.rpm")
+set -e
+
 mkdir -p ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot
-pushd ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot > /dev/null 2>&1
+RPMS=$(find "${SRC_DIR}"/binary -name "*.rpm" -printf "." | wc -c)
+RPM=$(find "${SRC_DIR}"/binary -name "*.rpm")
+TOTAL=$(find "${SRC_DIR}"/binary -type f -printf "." | wc -c)
+  pushd ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot > /dev/null 2>&1
+if [[ ${RPMS} == 1 ]] && [[ ${TOTALS} == 1 ]] && [[ -f ${RPM} ]]; then
   "${RECIPE_DIR}"/rpm2cpio "${RPM}" | cpio -idmv
+else
+  cp -Rf "${SRC_DIR}"/binary/* .
+fi
 popd > /dev/null 2>&1
