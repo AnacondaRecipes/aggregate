92724388047070a9c369fdf04a3af45216a2c35b
Ray Donnelly <mingw.android@gmail.com>
2018-07-02 19:01:03 +0100

    PowerPC CDTs: missing_dso_whitelist

--- xorg-x11-util-macros-cos7-ppc64le/build.sh
+++ xorg-x11-util-macros-cos7-ppc64le/build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-RPM=$(find ${PWD}/binary -name "*.rpm")
+set -e
+
 mkdir -p ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot
+RPMS=$(find "${SRC_DIR}"/binary -name "*.rpm" -exec printf "." \; | wc -c | tr -d " ")
+THE_RPM=$(find "${SRC_DIR}"/binary -name "*.rpm")
+TOTAL=$(find "${SRC_DIR}"/binary -type f -exec printf "." \; | wc -c | tr -d " ")
 pushd ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot > /dev/null 2>&1
-  "${RECIPE_DIR}"/rpm2cpio "${RPM}" | cpio -idmv
+  if [[ ${RPMS} == 1 ]] && [[ ${TOTAL} == 1 ]] && [[ -f ${THE_RPM} ]]; then
+    "${RECIPE_DIR}"/rpm2cpio "${THE_RPM}" | cpio -idmv
+  else
+    cp -Rf "${SRC_DIR}"/binary/* .
+  fi
 popd > /dev/null 2>&1
