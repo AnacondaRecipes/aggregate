d2d0b210377cbf14e776a18a981e9c69d5ca5844
Ray Donnelly <mingw.android@gmail.com>
2018-07-03 09:23:30 +0100

    PowerPC CDTs: Work around tar symlink length limitation (at unknown cost)

--- java-1.7.0-openjdk-devel-cos7-ppc64le/build.sh
+++ java-1.7.0-openjdk-devel-cos7-ppc64le/build.sh
@@ -13,3 +13,12 @@ pushd ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot > /dev/null 2>&1
     cp -Rf "${SRC_DIR}"/binary/* .
   fi
 popd > /dev/null 2>&1
+
+
+pushd ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot > /dev/null 2>&1
+  pushd ./usr/share/systemtap/tapset/powerpc > /dev/null 2>&1
+    # These files end up with relative symlinks that exceed the tar maximum length of 100. Not much
+    # we can do here, what should cb do when asked to package the impossible?
+    rm -f *.stp
+  popd
+popd
