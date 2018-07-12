4272844cf9943689724cfc4cf603f29673884c5c
Michael Sarahan <msarahan@gmail.com>
2018-07-12 08:23:57 -0500

    removing folders in preparation for moving to submodules

--- h2o-feedstock/recipe/bundle_python.sh
+++
@@ -1,6 +0,0 @@
-cd h2o-py
-# patch current version into
-H2O_BUILD_PY=true ../gradlew -PdoPython -Ph2o.web.allow.root build
-
-# gradle keeps workers running. we need to kill them so that we can remove files.
-../gradlew --stop
--- h2o-feedstock/recipe/install_jar.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p $PREFIX/h2o_jar
-mv build/h2o.jar $PREFIX/h2o_jar/
