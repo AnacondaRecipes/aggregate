36cacc22dd4b2a5ad7877bf7c74f36cc546b0d39
Ray Donnelly <mingw.android@gmail.com>
2018-08-26 18:44:43 +0100

    Various low-stack (miniconda deps) updates including python 3.5.6

--- git_s_fe_checkout_pull_rebase.sh
+++ git_s_fe_checkout_pull_rebase.sh
@@ -13,16 +13,4 @@ echo path = ${path}
 echo sha1 = ${sha1}
 echo toplevel = ${toplevel}
 
-# There is a '--name-only' flag to 'git config', but no '--value-only' flag .. onward and awkward
-wanted_branch=$(git config -f ${toplevel}/.gitmodules --get-regexp "^submodule\.${name}\.branch$" | awk '{ print $2 }')
-if current_branch=$(git symbolic-ref --short -q HEAD); then
-  if [[ ${current_branch} != ${wanted_branch} ]]; then
-    git checkout -B ${wanted_branch} && git branch -u origin/${wanted_branch} && git pull --rebase
-  else
-    git branch -u origin/${wanted_branch} && git pull --rebase
-  fi
-else
-  # Not on any branch, you may have detached commits though, but you may have commits in ${wanted_branch} too
-  # .. TODO :: need to be careful with that.
-  git checkout -B ${wanted_branch} && git branch -u origin/${wanted_branch} && git pull --rebase
-fi
+git checkout -p .
