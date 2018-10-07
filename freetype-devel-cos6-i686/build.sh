141b0e84415ff359d81d7ea8c4438b216d2f2281
Ray Donnelly <mingw.android@gmail.com>
2018-10-07 20:11:55 +0100

    Update centos6 i686 and x84_64 CDT packages
    Now that libarchive can handle RPMs conda-build will extract them so
    we make the build.sh scripts able to handle both (for now, should go
    back and remove these non-libarchive code paths eventually.

--- freetype-devel-cos6-i686/build.sh
+++ freetype-devel-cos6-i686/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 RPM=$(find ${PWD}/binary -name "*.rpm")
-mkdir -p ${PREFIX}/i686-conda_cos6-linux-gnu/sysroot
-pushd ${PREFIX}/i686-conda_cos6-linux-gnu/sysroot > /dev/null 2>&1
+mkdir -p ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot
+pushd ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot > /dev/null 2>&1
+if [[ -n "${RPM}" ]]; then
   "${RECIPE_DIR}"/rpm2cpio "${RPM}" | cpio -idmv
-popd > /dev/null 2>&1
+  popd > /dev/null 2>&1
+else
+  mv "${SRC_DIR}"/* .
+fi
--- libx11-common-cos6-x86_64/build.sh
+++ libx11-common-cos6-x86_64/build.sh
@@ -3,5 +3,9 @@
 RPM=$(find ${PWD}/binary -name "*.rpm")
 mkdir -p ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot
 pushd ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot > /dev/null 2>&1
+if [[ -n "${RPM}" ]]; then
   "${RECIPE_DIR}"/rpm2cpio "${RPM}" | cpio -idmv
-popd > /dev/null 2>&1
+  popd > /dev/null 2>&1
+else
+  mv "${SRC_DIR}"/* .
+fi
