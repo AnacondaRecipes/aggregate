c634099bba98c0f5cea6903e228bd62353f5ce19
Nehal J Wani <nehaljw.kkd1@gmail.com>
2018-10-24 08:40:29 -0500

    Make recipe compatible with openssl 1.1.x

--- serf-feedstock/recipe/build.sh
+++ serf-feedstock/recipe/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+sed -i.bak 's/c89/c99/g' SConstruct
+
 scons APR="$PREFIX" APU="$PREFIX" OPENSSL="$PREFIX" ZLIB="$PREFIX" \
-    PREFIX="$PREFIX" CC="$CC" CPPFLAGS="$CPPFLAGS" LINKFLAGS="$LDFLAGS"
+    PREFIX="$PREFIX" CC="$CC" CPPFLAGS="$CPPFLAGS" LINKFLAGS="$LDFLAGS" CFLAGS="$CFLAGS"
 
 scons install
