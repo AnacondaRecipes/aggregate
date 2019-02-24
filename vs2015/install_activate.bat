2c4e56fd50e68d29dbb52d8319ff1dc6fb6a119a
Isuru Fernando <isuruf@gmail.com>
2019-02-24 09:57:29 -0600

    Use VC140_ON_VS2017 to decide on whether to use VS 2017 to compiler for vc=14.0

--- vs2015/install_activate.bat
+++ vs2015/install_activate.bat
@@ -4,6 +4,18 @@ set VER=14
 mkdir "%PREFIX%\etc\conda\activate.d"
 COPY "%RECIPE_DIR%\activate.bat" "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
 
+echo if "%%VC140_ON_VS2017%%" == "" ( >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
+
+IF "%cross_compiler_target_platform%" == "win-64" (
+  echo SET "CMAKE_GENERATOR=Visual Studio 15 2017 Win64" -T "v140" >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
+  echo CALL "%%VS2017_VCVARSALL%%" x64 -vcvars_ver=14.0 >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
+  ) else (
+  echo SET "CMAKE_GENERATOR=Visual Studio 15 2017" -T "v140" >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
+  echo CALL "%%VS2017_VCVARSALL%%" x86 -vcvars_ver=14.0 >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
+  )
+
+echo ) else ( >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
+
 IF "%cross_compiler_target_platform%" == "win-64" (
   set "target_platform=amd64"
   echo SET "CMAKE_GENERATOR=Visual Studio %VER% %YEAR% Win64" >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
@@ -13,3 +25,5 @@ IF "%cross_compiler_target_platform%" == "win-64" (
   )
 
 echo CALL "%%VSINSTALLDIR%%..\..\VC\vcvarsall.bat" %target_platform% >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
+
+echo ) >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
