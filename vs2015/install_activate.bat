c940dfb7a9affffaf2f5f91bfd48d8736eb0f092
Isuru Fernando <isuruf@gmail.com>
2019-03-02 13:38:25 -0600

    Fix vs2015 activate logic

--- vs2015/install_activate.bat
+++ vs2015/install_activate.bat
@@ -6,21 +6,6 @@ COPY "%RECIPE_DIR%\activate.bat" "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compile
 
 echo if "%%VC140_ON_VS2017%%" == "" ( >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
 
-:: If env variable VC140_ON_VS2017 is not empty, assume that there is a Visual Studio 2017 installation
-:: with vc14.0 toolchain installed. Set the CMAKE_GENERATOR to use Visual Studio 2017 with vc14.0 toolchain
-:: and activate the toolchain
-:: This env variable needs to be set in conda_build_config.yaml if this package is used in a conda recipe
-
-IF "%cross_compiler_target_platform%" == "win-64" (
-  echo SET "CMAKE_GENERATOR=Visual Studio 15 2017 Win64" -T "v140" >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
-  echo CALL "%%VSINSTALLDIR%%..\..\VC\vcvarsall.bat" x64 -vcvars_ver=14.0 >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
-  ) else (
-  echo SET "CMAKE_GENERATOR=Visual Studio 15 2017" -T "v140" >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
-  echo CALL "%%VSINSTALLDIR%%..\..\VC\vcvarsall.bat" x86 -vcvars_ver=14.0 >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
-  )
-
-echo ) else ( >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
-
 :: If env variable VC140_ON_VS2017 is empty, assume that there is a Visual Studio 2015 installation
 :: Set the CMAKE_GENERATOR and activate the toolchain
 
@@ -33,5 +18,21 @@ IF "%cross_compiler_target_platform%" == "win-64" (
   )
 
 echo CALL "%%VSINSTALLDIR%%..\..\VC\vcvarsall.bat" %target_platform% >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
+
+
+echo ) else ( >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
+
+:: If env variable VC140_ON_VS2017 is not empty, assume that there is a Visual Studio 2017 installation
+:: with vc14.0 toolchain installed. Set the CMAKE_GENERATOR to use Visual Studio 2017 with vc14.0 toolchain
+:: and activate the toolchain
+:: This env variable needs to be set in conda_build_config.yaml if this package is used in a conda recipe
+
+IF "%cross_compiler_target_platform%" == "win-64" (
+  echo SET "CMAKE_GENERATOR=Visual Studio 15 2017 Win64" -T "v140" >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
+  echo CALL "%%VSINSTALLDIR%%VC\Auxiliary\Build\vcvarsall.bat" x64 -vcvars_ver=14.0 >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
+  ) else (
+  echo SET "CMAKE_GENERATOR=Visual Studio 15 2017" -T "v140" >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
+  echo CALL "%%VSINSTALLDIR%%VC\Auxiliary\Build\vcvarsall.bat" x86 -vcvars_ver=14.0 >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
+  )
 
 echo ) >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
