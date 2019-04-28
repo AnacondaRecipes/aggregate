3ef3428ed02c0d633e2b5488c8622421766ada51
Isuru Fernando <isuruf@gmail.com>
2019-04-28 11:09:27 -0500

    Move echo calls outisde of if

--- vs2017/install_activate.bat
+++ vs2017/install_activate.bat
@@ -7,16 +7,16 @@ COPY "%RECIPE_DIR%\activate.bat" "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compile
 IF "%cross_compiler_target_platform%" == "win-64" (
   set "target_platform=amd64"
   set "CMAKE_GENERATOR=Visual Studio %VER% %YEAR% Win64"
-  echo pushd "%%VSINSTALLDIR%%" >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
-  echo CALL "VC\Auxiliary\Build\vcvars64.bat" >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
-  echo popd >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
+  set "bits=64"
 ) else (
   set "target_platform=x86"
   set "CMAKE_GENERATOR=Visual Studio %VER% %YEAR%"
-  echo pushd "%%VSINSTALLDIR%%" >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
-  echo CALL "VC\Auxiliary\Build\vcvars32.bat" >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
-  echo popd >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
+  set "bits=32"
 )
+
+echo pushd "%%VSINSTALLDIR%%" >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
+echo CALL "VC\Auxiliary\Build\vcvars%bits%.bat" >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
+echo popd >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
 
 echo IF "%%CMAKE_GENERATOR%%" == "" ( >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
 echo SET "CMAKE_GENERATOR=%CMAKE_GENERATOR%" >> "%PREFIX%\etc\conda\activate.d\vs%YEAR%_compiler_vars.bat"
