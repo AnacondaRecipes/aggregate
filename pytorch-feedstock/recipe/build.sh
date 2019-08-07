b87e31e1352a4189c8c4a7bfe6efe69204cc2a2d
Jonathan Helmus <jjhelmus@gmail.com>
2019-08-07 14:17:29 -0500

    pytorch: 1.1.0

--- pytorch-feedstock/recipe/build.sh
+++ pytorch-feedstock/recipe/build.sh
@@ -12,13 +12,13 @@ export TH_BINARY_BUILD=1
 export PYTORCH_BUILD_VERSION=$PKG_VERSION
 export PYTORCH_BUILD_NUMBER=$PKG_BUILDNUM
 
-if [[ ${cudatoolkit} == 8.0* ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.0;3.5;5.0;5.2+PTX;6.0;6.1"
-elif [[ ${cudatoolkit} == 9.0* ]]; then
+if [[ ${cudatoolkit} == 9.0* ]]; then
     export TORCH_CUDA_ARCH_LIST="3.0;3.5;5.0;5.2+PTX;6.0;6.1;7.0"
 elif [[ ${cudatoolkit} == 9.2* ]]; then
     export TORCH_CUDA_ARCH_LIST="3.0;3.5;5.0;5.2+PTX;6.0;6.1;7.0"
 elif [[ ${cudatoolkit} == 10.0* ]]; then
+    export TORCH_CUDA_ARCH_LIST="3.0;3.5;5.0;5.2+PTX;6.0;6.1;7.0;7.5"
+elif [[ ${cudatoolkit} == 10.1* ]]; then
     export TORCH_CUDA_ARCH_LIST="3.0;3.5;5.0;5.2+PTX;6.0;6.1;7.0;7.5"
 fi
 
@@ -29,4 +29,4 @@ export USE_STATIC_NCCL=1
 export MAGMA_HOME="${PREFIX}"
 export LIBRT=$(find ${BUILD_PREFIX} -name "librt.so")
 
-python setup.py install
+python  -m pip install . --no-deps -vv
