1f8b96ce28d91d9aad649155f03ab951af0f2445
angloyna <angela.gloyna@gmail.com>
2020-07-16 21:42:43 -0500

    save work

--- dm-tree/bld.bat
+++ dm-tree/bld.bat
@@ -1,9 +1,4 @@
 @echo on
+%LIBRARY_BIN%\bazel build -c opt //tree:_tree
 
-set MSYSTEM=MINGW%ARCH%
-set MSYS2_PATH_TYPE=inherit
-set CHERE_INVOKING=1
-
-
-bash -lc "%RECIPE_DIR%"/build_win.sh
-if errorlevel 1 exit 1
+python setup.py install
