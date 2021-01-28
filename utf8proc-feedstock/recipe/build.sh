b37f486f18da86d3d112f451e4b1ff255eef8ab4
Kai Tietz <ktietz@anaconda.com>
2021-01-28 12:58:19 +0100

    Add utf8proc recipe

---
+++ utf8proc-feedstock/recipe/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir build
+cd build
+
+cmake -G "Unix Makefiles" \
+  -DCMAKE_BUILD_TYPE="Release" \
+  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_POSITION_INDEPENDENT_CODE=1 \
+  -DBUILD_STATIC=1 \
+  -DBUILD_SHARED=1 \
+  "${SRC_DIR}"
+
+cmake --build .
+cmake --build . --target install
+
