ab6b179cfc8211961811e61d29f155e79c65bff7
Kai Tietz <ktietz@anaconda.com>
2021-01-28 13:17:10 +0100

    Update utf8proc to support unix

--- utf8proc-feedstock/recipe/build.sh
+++ utf8proc-feedstock/recipe/build.sh
@@ -7,8 +7,7 @@ cmake -G "Unix Makefiles" \
   -DCMAKE_BUILD_TYPE="Release" \
   -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
   -DCMAKE_POSITION_INDEPENDENT_CODE=1 \
-  -DBUILD_STATIC=1 \
-  -DBUILD_SHARED=1 \
+  -DBUILD_SHARED_LIBS=1 \
   "${SRC_DIR}"
 
 cmake --build .
