2975ce1a848d2c0db2cacfe50d464352cb1f65bf
Kai Tietz <ktietz@anaconda.com>
2021-08-20 08:25:44 +0200

    fix osx build and add osx-arm64 support

--- serf-feedstock/recipe/build.sh
+++ serf-feedstock/recipe/build.sh
@@ -2,6 +2,12 @@
 
 sed -i.bak 's/c89/c99/g' SConstruct
 
+if [[ "${target_platform}" == osx-* ]]; then
+  export CPPFLAGS="$CPPFLAGS -isysroot $CONDA_BUILD_SYSROOT"
+  export CFLAGS="$CPPFLAGS -isysroot $CONDA_BUILD_SYSROOT"
+  export LDFLAGS="$LDFLAGS -isysroot $CONDA_BUILD_SYSROOT"
+fi
+
 scons APR="$PREFIX" APU="$PREFIX" OPENSSL="$PREFIX" ZLIB="$PREFIX" \
     PREFIX="$PREFIX" CC="$CC" CPPFLAGS="$CPPFLAGS" LINKFLAGS="$LDFLAGS" CFLAGS="$CFLAGS"
 
