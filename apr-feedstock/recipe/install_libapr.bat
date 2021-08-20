2c50336c6a6e054e6b09912d0c587ca405545f8e
Kai Tietz <ktietz@anaconda.com>
2021-08-20 08:08:57 +0200

    update to apr to version 1.7.0, and add osx-arm64 support

---
+++ apr-feedstock/recipe/install_libapr.bat
@@ -0,0 +1,11 @@
+set SUBPKG=apr
+
+cmake -GNinja ^
+    -S%SRC_DIR%\%SUBPKG% ^
+    -B%SRC_DIR%\%SUBPKG%.build ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
+if errorlevel 1 exit 1
+
+cmake --build %SRC_DIR%\%SUBPKG%.build -- install
+if errorlevel 1 exit 1
