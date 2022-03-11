9e0f52021582b6438ad8cb4db81b5ccbf03dadfb
Anthony DiPietro <adipietro@anaconda.com>
2022-03-11 11:35:05 -0500

    nsis-feedstock: add AccessControl plug-in.

--- nsis-feedstock/recipe/bld.bat
+++ nsis-feedstock/recipe/bld.bat
@@ -1,4 +1,5 @@
 robocopy nsis-zip "%PREFIX%"\NSIS /S
 robocopy "%RECIPE_DIR%"\NSIS "%PREFIX%"\NSIS /S
+robocopy access-control\Plugins\i386-unicode "%PREFIX%"\NSIS\Plugins\x86-unicode /S
 :: Checking the ErrorLevel from robocopy is pointless.
 exit /b 0
