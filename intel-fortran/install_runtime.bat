4e6f239cfd35d05016a903b991f66672fed8fb67
Daniel Komisar <dkomisar@anaconda.com>
2022-09-14 16:23:07 -0400

    activation scripts and cbc

--- intel-fortran/install_runtime.bat
+++ intel-fortran/install_runtime.bat
@@ -1,18 +1,24 @@
 COPY %RECIPE_DIR%\LICENSE.txt LICENSE.txt
 
-IF %compiler_version:~0,4% GEQ 2016 (
-    set "INSTAlLDIR=C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_%compiler_version%\windows\"
+IF "%target_platform%" == "win-64" (
+    set "intel_platform=intel64"
 ) else (
-    set "INSTALLDIR=C:\Program Files (x86)\Intel\Composer XE 2013 SP1\"
+    set "intel_platform=ia32"
 )
 
-IF "%target_platform%" == "win-64" (
-  set "intel_platform=intel64"
-  ) else (
-  set "intel_platform=ia32"
-  )
+IF %compiler_version:~0,4% GEQ 2022 (
+    set "INSTALLDIR=C:\Program Files (x86)\Intel\oneAPI\compiler\%compiler_version%\windows\"
+	set "SUFFIX=_win"
+) else (
+    IF %compiler_version:~0,4% GEQ 2016 (
+        set "INSTALLDIR=C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_%compiler_version%\windows\"
+    ) else (
+        set "INSTALLDIR=C:\Program Files (x86)\Intel\Composer XE 2013 SP1\"
+    )
+	set "SUFFIX="
+)
 
-robocopy "%INSTALLDIR%\redist\%intel_platform%\compiler"  "%LIBRARY_BIN%" *.dll /E
+robocopy "%INSTALLDIR%\redist\%intel_platform%%SUFFIX%\compiler" "%LIBRARY_BIN%" *.dll /E
 if %ERRORLEVEL% GEQ 8 exit 1
 IF EXIST %LIBRARY_BIN%\irml\irml_debug.dll DEL %LIBRARY_BIN%\irml\irml_debug.dll
 IF EXIST %LIBRARY_BIN%\irml_c\irml_debug.dll DEL %LIBRARY_BIN%\irml_c\irml_debug.dll
