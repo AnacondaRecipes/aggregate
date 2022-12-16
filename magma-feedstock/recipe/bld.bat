ed1624cf6ecb63e213ad04a3b163a686a21fb183
Sumit Tiwari <8823156+sumit0190@users.noreply.github.com>
2022-12-16 16:56:03 -0600

    fix windows build 1

--- magma-feedstock/recipe/bld.bat
+++ magma-feedstock/recipe/bld.bat
@@ -1,12 +1,23 @@
-cmake ^
-  -H. ^
-  -Bbuild ^
-  -G"Ninja" ^
+md build
+cd build
+if errorlevel 1 exit /b 1
+
+cmake %SRC_DIR%^
+  -G "Ninja" ^
   -DUSE_FORTRAN=OFF ^
+  -DMAGMA_ENABLE_CUDA=ON ^
   -DCMAKE_BUILD_TYPE=Release ^
+  -DGPU_TARGET="Fermi Kepler Maxwell Pascal Volta Turing Ampere" ^
   -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
+  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+  -DCUDA_NVCC_FLAGS="--use-local-env" ^
+if errorlevel 1 exit /b 1
 
-cmake --build build
+cmake --build . ^
+      --config Release ^
+      --parallel %CPU_COUNT% ^
+      --target magma magma_sparse ^
+      --verbose
+if errorlevel 1 exit /b 1
 
-cmake --build build -- install
+cmake --install .
