19a76fbaa3cfffca4f8947e2252d2b2815cd9714
Sumit Tiwari <8823156+sumit0190@users.noreply.github.com>
2022-12-20 12:08:02 -0600

    add mkl flag to control mkl threads

--- magma-feedstock/recipe/bld.bat
+++ magma-feedstock/recipe/bld.bat
@@ -1,19 +1,20 @@
 md build
-cd build
 if errorlevel 1 exit /b 1
 
-cmake %SRC_DIR%^
+cmake ^
+  -S . ^
+  -B build
   -G "Ninja" ^
   -DUSE_FORTRAN=OFF ^
   -DMAGMA_ENABLE_CUDA=ON ^
   -DCMAKE_BUILD_TYPE=Release ^
   -DGPU_TARGET="Fermi Kepler Maxwell Pascal Volta Turing Ampere" ^
-  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+  -DMAGMA_WITH_MKL ^
   -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
   -DCUDA_NVCC_FLAGS="--use-local-env" ^
 if errorlevel 1 exit /b 1
 
-cmake --build . ^
+cmake --build build ^
       --config Release ^
       --parallel %CPU_COUNT% ^
       --target magma magma_sparse ^
