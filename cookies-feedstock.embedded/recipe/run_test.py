c5e1d045ab6d65acbf80955b0a057e6965e9dd87
Ryan Keith <rkeith@anaconda.com>
2023-09-21 13:53:22 -0700

    Remove root feedstocks that were marked embedded.

--- cookies-feedstock.embedded/recipe/run_test.py
+++
@@ -1,7 +0,0 @@
-# new cookies.py
-from cookies import Cookies, Cookie
-
-cookies = Cookies(rocky='road')
-# Can also write explicitly: cookies['rocky'] = Cookie['road']
-cookies['rocky'].path = "/cookie"
-assert cookies.render_request() == 'rocky=road'
